0633c1bfe2fd4a88f79d729075fe1fc83aba409c
VNET\mnagah1 <mahmoud.nagah@valeo.com>
2021-09-10 20:01:08 +0200

    upload the chat bot submit to git hub

---
+++ src/graphnode.cpp
@@ -0,0 +1,48 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(std::move(edge));
+}
+/*change it to be smar pointer parameter*/
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move (edge));
+}
+
+
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = std::move(chatbot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+  //  _chatBot = nullptr;  will be done inside move
+}
+
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    
+/*get tthe index */
+    return _childEdges[index].get();
+}
